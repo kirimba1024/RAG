464940b27feee27881df4099190ad0b851e1731f
kirimba1024 <kirimba1024@yandex.com>
2025-11-11 21:25:20 +0300

    SAFE SANDBOX

--- images/sandbox/entrypoint.sh
+++ images/sandbox/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "[sandbox] Starting RAG sandbox..."
 echo "[sandbox] Monorepo ready at /app/monorepo"
-echo "[sandbox] Available commands: grep, find, awk, sed, bash, curl, wget, jq, tree, file, diffutils"
+echo "[sandbox] Available commands: grep, find, awk, sed, bash, jq, tree, file, diffutils"
 echo "[sandbox] Sandbox ready! Use: docker exec -it <container> sh"
 
 # Держим контейнер живым
